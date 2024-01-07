7bc4f4d5296676bb5423521839e4090484be3350
Amakuser <fzalilov28@gmail.com>
2024-01-07 22:37:14 +0300

    Изменяю на рабочий код

--- PythonBot_Telegram.py
+++ PythonBot_Telegram.py
@@ -1,10 +1,45 @@
+import threading
+#import telebot
+import time
+import sys
+import os
+import glob
+import random
 from telebot import TeleBot
+from telebot import types
 
 TOKEN = '6957693939:AAHO1RvZU8IUnRjL7CvUclI7j7mMSk7CESA'
 bot = TeleBot(TOKEN)
 
+def stop_bot():
+    print("Press 'N' to stop the bot")
+    while True:
+        if input() == 'N':
+            bot.stop_polling()
+            print("Bot has been stopped")
+            break
+
 @bot.message_handler(commands=["start"])
 def start(message):
     bot.send_message(message.chat.id, 'Hello!')
+    
+@bot.message_handler(commands=["give"])
 
-bot.polling(none_stop=True, interval=5)
+def send_variable_image(message):
+    path = "E:/JPG"
+    photo_list = glob.glob(os.path.join(path, '*.jpg'))
+    chat_id = message.chat.id
+    photo = random.choice(photo_list)
+    with open(photo, 'rb') as photo:
+        bot.send_photo(chat_id, photo)
+
+
+
+# Start polling in a separate thread
+polling_thread = threading.Thread(target=bot.polling, args=(True, 5))
+polling_thread.daemon = True
+polling_thread.start()
+
+# Start the stop_bot thread
+stop_bot_thread = threading.Thread(target=stop_bot)
+stop_bot_thread.start()
